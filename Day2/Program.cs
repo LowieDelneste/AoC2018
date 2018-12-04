3a35610d07b6b73c623d1d9cddf8db8f5d742849
LowieDelneste <LowieDelneste@users.noreply.github.com>
2018-12-04 21:07:59 +0100

    Day2 part 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -23,16 +23,33 @@ namespace Day2
             var exactTwo = 0;
             var exactThree = 0;
 
-            foreach (var id in ids)
+            ids.ForEach(id =>
             {
                 var grouped = id.GroupBy(x => x).ToList();
                 exactTwo += grouped.Any(x => x.Count() == 2) ? 1 : 0;
                 exactThree += grouped.Any(x => x.Count() == 3) ? 1 : 0;
-
-            }
+            });
 
             Console.WriteLine($"Checksum {exactTwo * exactThree}");
 
+            // Part 2
+            string commonLetters =  "";
+            foreach (var firstId in ids)
+            {
+                foreach (var secondId in ids)
+                {
+                    if (firstId == secondId) continue;
+                    var currentCommon = firstId.Where((letter, index) => letter == secondId[index]);
+                    if (currentCommon.Count() == firstId.Count() - 1)
+                    {
+                        commonLetters = string.Concat(currentCommon);
+                        break;
+                    }
+                }
+                if (!string.IsNullOrEmpty(commonLetters)) break;
+            }
+
+            Console.WriteLine($"Common: {commonLetters}");
 
             Console.ReadLine();
         }
